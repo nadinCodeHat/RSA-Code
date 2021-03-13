a71018162198a6a155bb42f1b5994947b2c61732
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-13 20:41:35 +0530

    Updated RSA-Code.cpp

--- RSA-Code/RSA-Code.cpp
+++ RSA-Code/RSA-Code.cpp
@@ -2,8 +2,14 @@
 
 int main()
 {
-
 	//Two Prime Numbers
-	//int x = , y = ;
+	int x = 73 , y = 97;
+	int n = x * y;
+
+	//Totient function (phi)
+	int phi = (x - 1) * (y - 1);
+
+	//e value co-prime of phi such that 1 < e < phi(n)
+	int e = 67;
 
 }
