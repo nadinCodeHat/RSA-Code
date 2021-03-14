4c5a8a4653ef994832d1386c9ab0eacba129b063
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-14 18:37:19 +0530

    Updated RSA-Code.cpp

--- RSA-Code/RSA-Code.cpp
+++ RSA-Code/RSA-Code.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
 int main()
 {
 	//Two Prime Numbers
-	int x = 73 , y = 97;
-	int n = x * y;
+	double x = 73 , y = 97;
+	double n = x * y;
 
 	//Totient function (phi)
-	int phi = (x - 1) * (y - 1);
+	double phi = (x - 1) * (y - 1);
 
 	//e value co-prime of phi such that 1 < e < phi(n)
-	int e = 67;
+	//public key
+	double encrypt = 67;
 
+	//Calculate d such that e.d = 1 mod phi(n)
+	//pair (n,d) makes up the private key
+	//private key
+	double decrypt = fmod(1 / encrypt, phi);
+	
+	//Plain Text
+	double message = 346;
+
+	//Encrypt the message
+	double encrypted_message = pow(message, encrypt);
+
+	//Decrypt the message
+	double decryped_message = pow(encrypted_message, decrypt);
+
+	encrypted_message = fmod(encrypted_message, n);
+	decryped_message = fmod(decryped_message, n);
+
+	cout << "Plain Text: " << message<<"\n";
+	cout << "Encrypted Text: " << encrypted_message << "\n";
+	cout << "Decrypted Text: " << decryped_message;
+
+	return 0;
 }
